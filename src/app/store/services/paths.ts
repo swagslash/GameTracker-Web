54ef0eb286babd3132cdfc9c297d5781058048ab
Andreas Tscheinig <andreas-tscheinig@gmx.at>
2020-01-19 22:05:25 +0100

    Correct API paths

--- src/app/store/services/paths.ts
+++ src/app/store/services/paths.ts
@@ -4,11 +4,9 @@ export const AUTH_PATH = `${API_BASE_PATH}/auth`;
 export const LOGIN_PATH = `${AUTH_PATH}/login`;
 export const SIGNUP_PATH = `${AUTH_PATH}/signup`;
 
-// TODO: change to actual endpoint path
-export const GAMES_PATH = `${API_BASE_PATH}/games`;
+export const WEBAPP_BASE_PATH = `${API_BASE_PATH}/webapp`;
+export const GAMES_BASE_PATH = `${WEBAPP_BASE_PATH}/games`;
 
-// TODO: change to actual endpoint path
-export const FETCH_GAMES_PATH =  `${GAMES_PATH}/fetch`;
-
-// TODO: change to actual endpoint path
-export const ADD_GAMES_PATH =  `${GAMES_PATH}/add`;
+export const GAMES_PATH = `${GAMES_BASE_PATH}/user`;
+export const FETCH_GAMES_PATH =  `${GAMES_BASE_PATH}/search`;
+export const ADD_GAMES_PATH =  `${GAMES_BASE_PATH}/add`;
