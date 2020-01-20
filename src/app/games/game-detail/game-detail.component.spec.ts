3dca6984825ce9ff21f4bf49f5064b35866da4c4
Christoph Wedenig <christoph@wedenig.org>
2020-01-20 19:05:12 +0100

    Fixed tests for game detail component

--- src/app/games/game-detail/game-detail.component.spec.ts
+++ src/app/games/game-detail/game-detail.component.spec.ts
@@ -1,6 +1,7 @@
 import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {GameDetailComponent} from './game-detail.component';
+import {GamesModule} from '../games.module';
 
 describe('GameDetailComponent', () => {
   let component: GameDetailComponent;
@@ -8,7 +9,8 @@ describe('GameDetailComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [GameDetailComponent]
+      declarations: [],
+      imports: [GamesModule]
     })
       .compileComponents();
   }));
@@ -16,10 +18,29 @@ describe('GameDetailComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(GameDetailComponent);
     component = fixture.componentInstance;
+    component.game = {
+      gameId: '720',
+      dbGameId: 'csgoid',
+      imageId: 'co1vce',
+      name: 'Counter-Strike Global Offensive',
+      tags: [{tagId: '1', name: 'Genre: Shooter', slug: 'shooter'}]
+    };
     fixture.detectChanges();
+  });
+
+  afterEach(() => {
+    fixture.destroy();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display the name', () => {
+    expect(fixture.nativeElement.querySelector('.game-name').innerText).toEqual('Counter-Strike Global Offensive');
+  });
+
+  it('should display the correct cover art', () => {
+    expect(fixture.nativeElement.querySelector('img').src).toEqual('https://images.igdb.com/igdb/image/upload/t_cover_big/co1vce.jpg');
   });
 });
