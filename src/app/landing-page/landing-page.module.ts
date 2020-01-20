41d9cc30a2517b0d87024b548cfe3fda9e21c596
Fabian Stiegler <fastiegler@edu.aau.at>
2020-01-21 00:39:57 +0100

    Created first landing page layout; added footer again; added toolbar shadow effect

--- src/app/landing-page/landing-page.module.ts
+++ src/app/landing-page/landing-page.module.ts
@@ -1,10 +1,16 @@
 import {NgModule} from '@angular/core';
 import {LandingPageRoutingModule} from './landing-page-routing.module';
 import {LandingPageComponent} from './landing-page.component';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 @NgModule({
   imports: [
     LandingPageRoutingModule,
+    MatGridListModule,
+    MatDividerModule,
+    MatToolbarModule,
   ],
   declarations: [
     LandingPageComponent
